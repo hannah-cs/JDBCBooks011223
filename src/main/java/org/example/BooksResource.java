67ef71161073ec16d694f976bff26b4c7db96684
hcs <hannahemcs@gmail.com>
2023-12-01 10:35:25 +0100

    Create (empty) methods for BooksResource

--- src/main/java/org/example/BooksResource.java
+++ src/main/java/org/example/BooksResource.java
@@ -2,5 +2,45 @@ package org.example;
 import javax.ws.rs.*;
 @Path("/books")
 public class BooksResource {
+    @GET
+    @Produces("application/json")
+    public void getBooks(){
+
+    }
+    @GET
+    @Path("/{id}")
+    @Produces("application/json")
+    public void getBook(int id){
+
+    }
+    @GET
+    @Path("/{title}")
+    @Produces("application/json")
+    public void getBooks(String title){
+
+    }
+    @GET
+    @Path("/{author}")
+    @Produces("application/json")
+    public void getBooks(String author){
+
+    }
+
+    @POST
+    @Consumes("application/json")
+    public void addBook(String bookData) {
+
+    }
+    @PUT
+    @Path("/{id}")
+    @Consumes("application/json")
+    public void updateBook(@PathParam("id") int id, String bookData) {
+
+    }
+    @DELETE
+    @Path("/{id}")
+    public void deleteBook(@PathParam("id") int id) {
+
+    }
 
 }
